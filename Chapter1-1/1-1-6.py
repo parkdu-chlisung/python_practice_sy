fd8acf269c96ed9ce16304fed226c314c4ad392b
seyxxn <lucy9480@gmail.com>
2022-03-02 21:13:11 +0900

    ** 문제 6 : False

---
+++ Chapter1-1/1-1-6.py
@@ -0,0 +1,18 @@
+# 문제 6 : False
+# 다음은 파이썬 문법 중에서 False로 취급하는 것들 입니다.
+# False로 취급하지 않는 것이 하나 있습니다. True를 찾아주세요.
+
+# 1) None
+# 2) 1 -> False
+# 3) ""
+# 4) 0
+# 5) bool(0)
+
+print(bool(None))
+print(bool(1))
+print(bool(""))
+print(bool(0))
+print(bool(0))
+
+# 1-> 참
+# 0을 제외한 모든 숫자는 참이다.
