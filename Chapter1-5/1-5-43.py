af2522c518c380df322f7e8ea96710fd114d8010
seyxxn <lucy9480@gmail.com>
2022-03-10 14:36:33 +0900

    * 문제 43 : 10진수를 2진수로

--- Chapter1-5/1-5-43.py
+++ Chapter1-5/1-5-43.py
@@ -1,1 +1,17 @@
 # 문제 43 : 10진수를 2진수로
+
+x = int(input())
+l = []
+
+while (True) :
+    if (x//2 != 0):
+        l.append(int(x%2))
+        x = x//2
+    elif (x//2 ==0):
+        l.append(int(x%2))
+        break
+
+l = list(reversed(l))
+
+for i in l:
+    print(i, end="")
